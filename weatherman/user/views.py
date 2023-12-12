6a685d89d5760c96eb5a35c8f5f8c33f4d067441
arahmaninov <arahmaninov@yandex.ru>
2023-12-12 16:25:39 +0300

    add env setting

--- weatherman/user/views.py
+++ weatherman/user/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 from .models import Users
 
 
 def show_main(request):
     context = {}
+    print(f"API token: {settings.OPENWEATHERMAP_API_TOKEN}")
 
     return render(request, "index.html", context)
