ee780f7f8ce07601670bf56a97ffd1fc15fe2078
arahmaninov <arahmaninov@yandex.ru>
2023-12-12 15:34:46 +0300

    add templates and views

--- weatherman/user/views.py
+++ weatherman/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Users
+
+
+def show_main(request):
+    context = {}
+
+    return render(request, "index.html", context)
